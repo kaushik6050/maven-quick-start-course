617ffc2733288c5328e00dc08b148497b797d543
EC2 Default User <ec2-user@ip-172-31-29-169.us-east-2.compute.internal>
2017-09-25 14:11:52 +0000

    package

--- src/main/java/clinic/programming/training/application.java
+++ src/main/java/clinic/programming/training/application.java
@@ -1,16 +1,49 @@
 package clinic.programming.training;
 
     
-         public class application {
-  public static void main(String[] args){
+ import java.util.Arraylist;
+import java.util.list;
+import org.apache.commons.lang3.StringUtils;       
 
-		int length=15,width=25;
-		int area=length*width;
-		double price=3.25,cost=price*area;
-		
-		System.out.println("The floor space is" +area +" square feet");
-		System.out.println(" Price of carpeting per square foot is "+cost);
-		}
 
-	  
-  }
+
+public class application {
+
+public int countWords(String words){
+	String[] seperateWords = StringUtils.split(words,' ');
+	return (seperateWords == null )? 0: seperateWords.length;
+
+
+
+
+public void greet(){
+List<String>greetings=new Arraylist<>();
+greetings.add("Hello");
+
+for(String greeting :greetings) {
+System.out.println("Grreting : " +greeting);
+}
+}
+    
+
+    public class application() {
+
+        System.out.println ("Inside Application");
+
+    }
+
+
+
+    // method main(): ALWAYS the APPLICATION entry point
+   
+         public static void main (String[] args) {
+    
+             	System.out.println ("Starting Application");
+    
+             		application app = new application();
+    app.greet();
+int count= app.countWords("I have four Words");
+System.out.println("Word Count  :" +count);
+             		    }
+    
+             		    }
