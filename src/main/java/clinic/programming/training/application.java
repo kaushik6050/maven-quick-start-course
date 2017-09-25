68bf2b76beda74da6d539722e485adc2994c8c00
EC2 Default User <ec2-user@ip-172-31-29-169.us-east-2.compute.internal>
2017-09-25 03:23:21 +0000

    Adding dummy file

---
+++ src/main/java/clinic/programming/training/application.java
@@ -0,0 +1,16 @@
+package clinic.programming.training;
+
+    
+         public class application {
+  public static void main(String[] args){
+
+		int length=15,width=25;
+		int area=length*width;
+		double price=3.25,cost=price*area;
+		
+		System.out.println("The floor space is" +area +" square feet");
+		System.out.println(" Price of carpeting per square foot is "+cost);
+		}
+
+	  
+  }
