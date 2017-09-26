36cd5eec9ed7de765ea462d5e55fd90206e3cbf1
EC2 Default User <ec2-user@ip-172-31-29-169.us-east-2.compute.internal>
2017-09-26 17:17:37 +0000

    back to reset defaults

--- src/main/java/clinic/programming/training/application.java
+++ src/main/java/clinic/programming/training/application.java
@@ -1,49 +1,36 @@
 package clinic.programming.training;
+import java.util.ArrayList;
 
-    
- import java.util.Arraylist;
-import java.util.list;
-import org.apache.commons.lang3.StringUtils;       
+	import java.util.List;
+ 
+	     
 
-
-
-public class application {
-
-public int countWords(String words){
-	String[] seperateWords = StringUtils.split(words,' ');
-	return (seperateWords == null )? 0: seperateWords.length;
+	public class application {
 
 
 
+              
+	
 
-public void greet(){
-List<String>greetings=new Arraylist<>();
-greetings.add("Hello");
 
-for(String greeting :greetings) {
-System.out.println("Grreting : " +greeting);
-}
-}
-    
+	public void greet(){
 
-    public class application() {
+	List<String>greetings=new ArrayList<>();
 
-        System.out.println ("Inside Application");
+	greetings.add("Hello");
 
+	for(String greeting :greetings) {
+
+	System.out.println("Greeting : " +greeting);
+	     	         	             	System.out.println ("Starting Application");
+	     	        	application app = new application();
+	       	app.greet();}
+  	         		
+	
+	     	         	             		             		             			             		    }
+	    
+	     	         	             		             		             			             		    	    
+	    
+	    
     }
-
-
-
-    // method main(): ALWAYS the APPLICATION entry point
-   
-         public static void main (String[] args) {
-    
-             	System.out.println ("Starting Application");
-    
-             		application app = new application();
-    app.greet();
-int count= app.countWords("I have four Words");
-System.out.println("Word Count  :" +count);
-             		    }
-    
-             		    }
+}
